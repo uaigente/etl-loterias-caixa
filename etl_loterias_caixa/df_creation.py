284389fa0dfb4ae73dda1e312eadd7124056e7ad
Gabriel Dornas <gabrielbdornas@gmail.com>
2026-01-14 19:25:41 -0300

    feat(datapackage): add datapackage
    and the mega_sena resource
    and a script to create dfs
    
    See #2

---
+++ etl_loterias_caixa/df_creation.py
@@ -0,0 +1,9 @@
+from frictionless import Package
+
+# package = Package('datapackage_csv.yaml')
+# resource = package.get_resource('mega_sena')
+# df = resource.to_pandas()
+
+package = Package('datapackage.yaml')
+resource = package.get_resource('mega_sena')
+df = resource.to_pandas()
