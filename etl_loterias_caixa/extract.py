9011c1e7ddc17b4063a0f316a3aeda4d95e631d6
Gabriel Dornas <gabrielbdornas@gmail.com>
2026-01-14 22:00:17 -0300

    feat(etl_loterias_caixa): create extract script
    See #3

---
+++ etl_loterias_caixa/extract.py
@@ -0,0 +1,53 @@
+import requests
+from frictionless import Package
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def extract_resource(resource):
+    """
+    Extract a isolated resource from a datapackage.
+    """
+    try:
+        sources = resource.sources
+        sources = [source for source in sources if source.get('method') is not None]
+        source = sources[0]
+        if source:
+            logger.info(f'Extracting resource {resource.name}.')
+            func = getattr(requests, source['method'])
+            # TODO: Pass parameters as **kwargs
+            response = func(
+                source['path'],
+                timeout=source['timeout'] if 'timeout' in source else 30,
+                params=source['params'] if 'params' in source else {},
+                )
+            response.raise_for_status()
+
+        with open(resource.path, 'wb') as f:
+            f.write(response.content)
+
+        logger.info(f'CSV file successfully downloaded to: {resource.path}.')
+        logger.info(f'File size: {len(response.content):,} bytes.')
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error downloading file: {e}")
+        raise
+    except IOError as e:
+        print(f"Error saving file: {e}")
+        raise
+
+
+def extract_resources(descriptor: str = 'datapackage.yaml'):
+    """
+    Extract resources from a datapackage.
+    """
+    logger.info(f'Extracting resources from {descriptor}.')
+    package = Package(descriptor)
+
+    for resource in package.resources:
+        extract_resource(resource)
+
+
+if __name__ == "__main__":
+    extract_resources()
