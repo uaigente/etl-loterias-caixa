9579229dfd2bdec0c81a70a2ad371c6166e76a69
Gabriel Dornas <gabrielbdornas@gmail.com>
2026-01-15 17:51:18 -0300

    feat(etl_loterias_caixa): add transform script
    See #5

--- etl_loterias_caixa/transform.py
+++ etl_loterias_caixa/transform.py
@@ -4,16 +4,33 @@ from pipelines import transform_pipeline
 
 logger = logging.getLogger(__name__)
 
-def transform_resource(resource_name: str, source_descriptor: str = 'datapackage.yaml'):
-    logger.info(f'Transforming resource {resource_name}')
+def transform_resource(resource):
+    """
+    Transform a isolated resource from a datapackage.
+    """
+    logger.info(f'Transforming resource {resource.name}')
 
-    package = Package(source_descriptor)
-    resource = package.get_resource(resource_name)
-    resource.transform(transform_pipeline)
-    table = resource.to_pandas()
-    breakpoint()
-    # for field in resource.schema.fields:
-    #     target = field.custom.get('target')
-    #     target = target if target else field.name.replace(' ', '_').lower()
-    #     table = etl.rename(table, field.name, target)
-    # etl.tocsv(table, f'data/{resource.name}.csv.gz', encoding='utf-8')
+    df = resource.to_pandas()
+    for field in resource.schema.fields:
+        target = field.custom.get('target')
+        target = target if target else field.name.replace(' ', '_').lower()
+        df.rename(columns={field.name: target}, inplace=True)
+
+    df.to_csv(f'data/{resource.name}.csv',
+              index=False,
+              sep=',',
+              encoding='utf-8',
+    )
+
+def transform_resources(descriptor: str = 'datapackage.yaml'):
+    """
+    Transform resources from a datapackage.
+    """
+    logger.info(f'Extracting resources from {descriptor}.')
+    package = Package(descriptor)
+
+    for resource in package.resources:
+        transform_resource(resource)
+
+if __name__ == '__main__':
+    transform_resources()
